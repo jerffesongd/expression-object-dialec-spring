6754cf3f7ea5a8416ea137d0b4c0bc4f36eaeda4
Jerffeson Gomes <jerffesongd@gmail.com>
2020-08-27 21:39:03 -0300

    change to show 'is even' or 'is not even'

--- src/main/java/com/example/expressionobjectdialect/expression/NumberUtils.java
+++ src/main/java/com/example/expressionobjectdialect/expression/NumberUtils.java
@@ -4,22 +4,12 @@ package com.example.expressionobjectdialect.expression;
 public final class NumberUtils {
 
 
-	public boolean isEven(Integer number) {
+	public String isEven(Integer number) {
 		
 		if (number % 2 == 0) {
-			return true;
+			return number +" is even";
 		} else {
-			return false;
-		}
-		
-	}
-
-	public boolean arePairs(Integer number1, Integer number2) {
-		
-		if (number1 % 2 == 0 && number2 % 2 == 0) {
-			return true;
-		} else {
-			return false;
+			return number + " is not even";
 		}
 		
 	}
